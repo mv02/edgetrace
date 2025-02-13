0cffb78b776e8ea6be6f6ebc3195eff67057f981
Milan Vodák <milanvodak02@gmail.com>
2025-02-13 22:36:59 +0100

    feat: add method properties panel

---
+++ frontend/src/lib/DataField.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    label: string;
+    children?: Snippet;
+    element?: string;
+  }
+
+  const { label, children, element = "h6" }: Props = $props();
+</script>
+
+<div class="flex flex-col gap-1">
+  <svelte:element this={element} class="text-wrap text-xs font-bold uppercase text-gray-500">
+    {label}
+  </svelte:element>
+
+  <p class="break-all text-sm">
+    {#if children}
+      {@render children()}
+    {:else}
+      -
+    {/if}
+  </p>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from "svelte";
   import { PUBLIC_API_URL } from "$env/static/public";
   import type { PageProps } from "./$types";
+  import DataField from "$lib/DataField.svelte";
   import TreeView from "$lib/TreeView.svelte";
   import { Button, Spinner } from "flowbite-svelte";
 
@@ -11,9 +12,13 @@
 
   let container: HTMLElement;
   let loading = $state(false);
+  let selectedNode: cytoscape.NodeSingular | undefined = $state();
 
   onMount(() => {
     cy.mount(container);
+    cy.on("tap", "node", (e: cytoscape.EventObject) => {
+      selectedNode = e.target;
+    });
   });
 
   const test = async () => {
@@ -27,6 +32,7 @@
 
   const clear = async () => {
     cy.removeAll();
+    selectedNode = undefined;
   };
 </script>
 
@@ -51,6 +57,17 @@
   </aside>
 
   <section class="flex-grow overflow-hidden" bind:this={container}></section>
+
+  <aside
+    class="flex flex-col gap-4 border-l-2 border-l-gray-200 p-4 lg:w-80 dark:border-l-gray-800"
+  >
+    {#if selectedNode}
+      <h3>Method Properties</h3>
+      {#each ["Type", "Name", "Parameters", "Return", "Id"] as key}
+        <DataField label={key}>{selectedNode.data(key).replaceAll(" ", ", ")}</DataField>
+      {/each}
+    {/if}
+  </aside>
 </main>
 
 <svelte:head>
