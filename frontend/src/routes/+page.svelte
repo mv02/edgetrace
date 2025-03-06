609116622361ce151f1c4a32a3cfe8e4e8fe250b
Milan Vodák <milanvodak02@gmail.com>
2025-03-06 12:32:55 +0100

    feat: add basic graphs dashboard

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Card } from "flowbite-svelte";
+
+  let { data } = $props();
+</script>
+
+<main class="container mx-auto grid auto-rows-min grid-cols-4 gap-4 p-8">
+  {#each data.graphs as graph}
+    <Card href="/graphs/{graph.name}">
+      <h2 class="font-semibold text-gray-900 dark:text-white">
+        <i class="nf nf-md-graph_outline"></i>
+        {graph.name}
+      </h2>
+      <p>{graph.nodeCount} nodes</p>
+    </Card>
+  {/each}
+</main>
